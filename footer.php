065d27a298a7a87b1c6a416b6d017de6e2d25ad0
Sam Solomon <sam@solomon.io>
2014-11-18 15:54:18 -0600

    added footer

---
+++ footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Theme Footer
+ *
+ * @version 1.0.0
+ * @package Upvote
+ */
+?>
+
+		</div><!-- /.container -->
+
+<footer>
+	<div class="container">		
+		<p>
+			Copyright &copy; Solomon &amp; Company <?php echo date("Y") ?>. All rights reserved.
+		</p>
+
+		<?php wp_nav_menu( array('menu' => 'Footer' )); ?>
+	
+	</div>
+
+
+<?php wp_footer(); ?>
+
+
+</footer>
+
+</body>
+</html>
