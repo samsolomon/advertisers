b8dcb1a58b3edfe28b2fb89cf404d67c7e143d46
Sam Solomon <sam@solomon.io>
2014-11-22 18:48:20 -0600

    updated search form

---
+++ searchform.php
@@ -0,0 +1,11 @@
+<div class="row">
+	
+<form role="form" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<div>
+		<label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
+		<input type="text" class="form-control search-box" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+		<input type="submit" class="btn btn-default" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+	</div>
+</form>
+
+</div>
---
+++ templates/404.php
@@ -0,0 +1,5 @@
+<article id="post-0" class="post error404 no-results not-found">
+	<h1 class="entry-title">Not Found</h1>
+	<p>It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help</p>
+	<?php get_search_form(); ?>
+</article>
